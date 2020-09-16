64d4837a493504c078e089153e467cd36e1c388b
open-antux <antonio.giustino11@gmail.com>
2020-09-16 07:36:08 +0200

    Fix stat.js

--- comandi/stat.js
+++ comandi/stat.js
@@ -5,16 +5,27 @@ module.exports = {
   name: 'stat',
   description: 'Mostra le informazioni del server',
   execute(msg, args){
+    let string = "";
     
     // Faccio una richiesta di tipo get per avere il file JSON
     https.get("https://api.mcsrvstat.us/2/mc.perecraft.ml", (res) => {
       
       // Converto il responso in UTF-8
       res.setEncoding("utf8");
-      
+
       res.on('data', (chunk) => {
-        // Leggo il file JSON e lo metto in out
-        let json = JSON.parse(chunk);
+        // Prendo ogni chunk per poi passato a JSON.parse()
+        string += chunk;
+      })
+      
+      res.on('end', () => {
+        // Leggo il file JSON
+        try{
+          let json = JSON.parse(string);
+        }catch (err){
+          console.log(err);
+          msg.channel.send("C'è stato un errore alla lettura del file, siete pregati di segnalare l'errore.");
+        }
 
         let flagOnline = (json.online) ? "online" : "offline";
         /*
@@ -34,7 +45,9 @@ status: ${flagOnline}
         `
         );
       })
-    })
+    }).on("error", (err) => {
+      msg.channel.send("C'è stato un errore interno, siete pregati di segnalare l'errore.");
+    });
   },
 };
 
